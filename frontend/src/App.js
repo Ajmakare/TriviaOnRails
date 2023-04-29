ab1088b5adfbec2ca98a9fc1bfd3e0a7754dc275
Aidan Makare <aidanmakaree@gmail.com>
2023-04-29 12:38:43 -0500

    Leaderboard componenet location change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Home from './components/Home';
 import Game from './components/Game';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Leaderboard from './components/Leaderboard';
 import './App.css';
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
         </Routes>
-        <Leaderboard />
         <Footer />
       </div>
     </Router>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Home.css';
 import { useNavigate } from 'react-router-dom';
+import Leaderboard from './Leaderboard';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -14,7 +15,9 @@ const Home = () => {
       <h2>Welcome to Trivia on Rails!</h2>
       <p>A state of the art (VERY difficult) trivia application built with Ruby on Rails and React</p>
       <button style = {{margin:'auto'}} className="play-now-button" onClick={handlePlayNowClick}>Play Now</button>
+      <Leaderboard />
     </div>
+
   );
 };
 
